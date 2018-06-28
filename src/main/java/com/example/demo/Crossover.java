d9003a526b0e7a88de2ed660a34829fe8d4b0c02
Serdar Turkmen <baver_t@hotmail.com>
2018-06-28 21:46:29 +0300

    cross

---
+++ src/main/java/com/example/demo/Crossover.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class Crossover {
+
+    public static void main(String... args) {
+
+        final Collection<? extends Number> foo = new ArrayList<Number>();
+
+        foo.add(null);
+
+        int i=1;
+        if(i++ == --i){
+            System.out.println(i);
+        }
+        else{
+            System.out.println("one");
+        }
+
+
+    }
+
+}
